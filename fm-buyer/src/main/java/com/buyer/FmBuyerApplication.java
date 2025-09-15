0f8870df73f5ac88109b664c9fab598fa7dbc8e4
Shubham Bhoskar <shubhambhoskar@gmail.com>
2025-09-15 19:20:54 +0530

    project setup

---
+++ fm-buyer/src/main/java/com/buyer/FmBuyerApplication.java
@@ -0,0 +1,13 @@
+package com.buyer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmBuyerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmBuyerApplication.class, args);
+	}
+
+}
---
+++ fm-buyer/src/test/java/com/buyer/FmBuyerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buyer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmBuyerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
